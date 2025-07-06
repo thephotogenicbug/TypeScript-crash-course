b4b32e36a2d8d166e56cf4e75abec7de3fb8c3c1
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-06 16:46:13 +0530

    primitive-types

---
+++ primitive-types.js
@@ -0,0 +1,7 @@
+"use strict";
+// Primitive Types
+let x; // explicite type
+x = 2;
+let y = 2; // implicite type
+let a;
+a = "hello";
---
+++ primitive-types.ts
@@ -0,0 +1,21 @@
+let x: number; // explicite type
+x = 2;
+let y = 2; // implicite type
+
+let a: string;
+a = "hello";
+
+let result = null;
+result = 5;
+result = "hello";
+
+let result1: number | undefined = undefined; // union operator
+result1 = 5;
+
+// number
+// string
+// boolean
+// undefined
+// null
+// void
+// never
