219b269072fd8a78c33884d0058495c246affcb7
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-10 15:23:18 +0530

    Type Gaurds

---
+++ typeGaurds.ts
@@ -0,0 +1,51 @@
+// typeof
+// instanceof
+// in
+
+type StringOrNumber1 = string | number;
+
+function add1(value: StringOrNumber1): StringOrNumber1 {
+  if (typeof value === "string") {
+    return value + "1";
+  } else {
+    return value + 1;
+  }
+}
+
+// example
+class Dog1_ {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+class Cat1_ {
+  firstName: string;
+  constructor(firstName: string) {
+    this.firstName = firstName;
+  }
+}
+
+// custom type
+function isDog(pet: Dog1_ | Cat1_): pet is Dog1_ {
+  return (pet as Dog1_).lastName !== undefined;
+}
+
+function getName(animal: Cat1_ | Dog1_) {
+  if (animal instanceof Cat1_) {
+    console.log("The name is ", animal.firstName);
+  } else {
+    console.log("The name is", animal.firstName + " " + animal.lastName);
+  }
+}
+
+function getName1(animal: Cat1_ | Dog1_) {
+  if ("lastName" in animal) {
+    console.log("The name is", animal.firstName + " " + animal.lastName);
+  } else {
+    console.log("The name is ", animal.firstName);
+  }
+}
