c3361175b756d57d24bfe679eedd73ef3b2837a7
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-10 16:06:41 +0530

    namespace

---
+++ nameSpace.ts
@@ -0,0 +1,11 @@
+namespace Utils {
+  export class MyClass {}
+
+  export function myFunction() {}
+  export const NAME = "Naveen";
+  export interface NewType {
+    name: string;
+  }
+}
+
+const result1_ = Utils.myFunction();
